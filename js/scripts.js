84dbb3e0f07820493b006b5a7bd721e29437f1c2
marovski <cardozo27cv@gmail.com>
2018-04-22 09:47:03 +0100

    Initial Commit
    Basic upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+//Filtering Recents Works Script
+
+    $(".filter-button").on("click", function () {
+        "use strict";
+        var value = $(this).attr("data-filter");
+        
+        if (value === "all")
+        {
+            $(".filter").show("1000");
+        }
+        else
+        {
+            $(".filter").not("." +value).hide("3000");
+            $(".filter").filter("." +value).show("3000");     
+        }
+
+        if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+        }
+            $(this).addClass("active");
+
+    });
