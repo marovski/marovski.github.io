0300a776dda200ebff5b5d32f3845a73ee5a3bcc
marovski <cardozo27cv@gmail.com>
2018-04-25 13:10:28 +0100

    New changes Holiday 25/04 PT

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,69 @@
+$(function() {
+
+
+  
+
+    //ADDED FROM Brittany Chiang
+  //   function myFunction(x) {
+
+
+  //     document.querySelector(".example").style.backgroundColor = "red";
+  // }
+
+  const welcomeAHeight = document.querySelector('.header').offsetHeight;
+  const topButton = document.getElementById('top-button');
+  const $topButton = $('#top-button');
+
+  window.addEventListener(
+    'scroll',
+    function() {
+      if (window.scrollY > introHeight) {
+        $topButton.fadeIn();
+      } else {
+        $topButton.fadeOut();
+      }
+    },
+    false
+  );
+
+  //SCROLL DOWN
+topButton.addEventListener('click', function() {
+      $('html, body').animate({ scrollTop: 750 }, 0 );
+    });  
+  
+
 
 //Filtering Recents Works Script
 
+
     $(".filter-button").on("click", function () {
-        "use strict";
-        var value = $(this).attr("data-filter");
-        
-        if (value === "all")
-        {
-            $(".filter").show("1000");
-        }
-        else
-        {
-            $(".filter").not("." +value).hide("3000");
-            $(".filter").filter("." +value).show("3000");     
-        }
+      "use strict";
+      var value = $(this).attr("data-filter");
+      
+      if (value === "all")
+      {
+          $(".filter").show("1000");
+      }
+      else
+      {
+          $(".filter").not("." +value).hide("3000");
+          $(".filter").filter("." +value).show("3000");     
+      }
 
-        if ($(".filter-button").removeClass("active")) {
-            $(this).removeClass("active");
-        }
-            $(this).addClass("active");
+      if ($(".filter-button").removeClass("active")) {
+          $(this).removeClass("active");
+      }
+          $(this).addClass("active");
 
-    });
+  });
+  });
 
 
-    //ADDED FROM Brittany Chiang
-    const introHeight = document.querySelector('.header').offsetHeight;
-    const topButton = document.getElementById('top-button');
-    const $topButton = $('#top-button');
+
+
+
+
+
   
-    window.addEventListener(
-      'scroll',
-      function() {
-        if (window.scrollY < introHeight) {
-          $topButton.fadeIn();
-        } else {
-          $topButton.fadeOut();
-        }
-      },
-      false
-    );
-
-    //SCROLL UP
-  topButton.addEventListener('click', function() {
-        $('html, body').animate({ scrollTop: 750 }, 0 );
-      });  
+  
     
