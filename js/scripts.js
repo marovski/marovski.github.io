2929e63c0d4a33280c12abe540d6d416f4d79b3a
marovski <cardozo27cv@gmail.com>
2018-04-26 01:43:07 +0100

    New changesNOHoliday 26/04 PT

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,40 @@
-$(function() {
-
-
-  
-
-    //ADDED FROM Brittany Chiang
-  //   function myFunction(x) {
-
-
-  //     document.querySelector(".example").style.backgroundColor = "red";
-  // }
-
-  const welcomeAHeight = document.querySelector('.header').offsetHeight;
-  const topButton = document.getElementById('top-button');
-  const $topButton = $('#top-button');
-
-  window.addEventListener(
-    'scroll',
-    function() {
-      if (window.scrollY > introHeight) {
-        $topButton.fadeIn();
-      } else {
-        $topButton.fadeOut();
-      }
-    },
-    false
-  );
-
-  //SCROLL DOWN
-topButton.addEventListener('click', function() {
-      $('html, body').animate({ scrollTop: 750 }, 0 );
-    });  
-  
-
+$(function () {
 
 //Filtering Recents Works Script
 
 
-    $(".filter-button").on("click", function () {
-      "use strict";
-      var value = $(this).attr("data-filter");
-      
-      if (value === "all")
-      {
-          $(".filter").show("1000");
-      }
-      else
-      {
-          $(".filter").not("." +value).hide("3000");
-          $(".filter").filter("." +value).show("3000");     
-      }
+$(".filter-button").on("click", function () {
+  "use strict";
+  var value = $(this).attr("data-filter");
 
-      if ($(".filter-button").removeClass("active")) {
-          $(this).removeClass("active");
-      }
-          $(this).addClass("active");
+  if (value === "all") {
+    $(".filter").show("1000");
+  }
+  else {
+    $(".filter").not("." + value).hide("3000");
+    $(".filter").filter("." + value).show("3000");
+  }
 
-  });
-  });
+  if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+
+});
 
 
 
 
 
+});
 
 
-  
-  
+
+
     
+
+
+
+
+
+
